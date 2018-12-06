f2b641252bc9ab5d84fd3f4febaa97271b8b3707
Max Kunkels <kunkelsmax@gmail.com>
2018-12-06 17:31:16 +0100

    Styling finished

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -5,11 +5,21 @@ import {
   Text,
   View,
   Button,
+  
 } from 'react-native';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Your Advice',
+    headerStyle: {
+      backgroundColor: '#FF4D18',
+    },
+    headerTintColor: 'white',
+    justifyAllignment: 'center',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+      flex: 1,
+    },
   };
   constructor(props) {
     super(props);
@@ -42,45 +52,39 @@ export default class LinksScreen extends React.Component {
       // console.log(humidity);
     }
     return (
-      <ScrollView style={styles.container}>
-        <View style={styles.mainContainer}>
-          <View className="container">
-            <View className="header">
-              <Text>Your Advice</Text>
-            </View>
-            <View className="content">
-              <Text className="data-content">
-                {isLoading ? 'loading.....' : response.main.temp}°C &nbsp;
-                        / &nbsp; {isLoading ? 'loading.....' : response.name} &nbsp;
-                        / &nbsp; {isLoading ? 'loading.....' : response.main.humidity}%
-                        </Text>
-              <Text>Shirt: {
+      <View style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.text}>Your Advice</Text>
+          </View>
+          <ScrollView contentContainerStyle={styles.mainContainer}>
+            <View>
+              <Text style={styles.text}>Shirt: {
                 (temp >= 15) ? 'Wear a T-shirt' :
                   (temp <= 15) ? 'You need a long shirt or a sweater' :
                     (temp <= 5) ? 'You need a thick sweater' :
                       'Might not want to bother going outside'
               }</Text>
-              <Text>Jacket: {
+              <Text style={styles.text}>Jacket: {
                 (temp >= 20) ? 'Leave your jackets at home!' :
                   (temp >= 15) ? 'Bring a summer jacket' :
                     (temp <= 8) ? 'Wear a winter jacket' :
                       (temp <= 15) ? 'You need your jacket' :
                         'Might not want to bother going outside'
               }</Text>
-              <Text>Jeans: {
+              <Text style={styles.text}>Jeans: {
                 (temp <= 5) ? 'You need pretty warm pants' :
                   (temp <= 20) ? 'Wear long jeans' :
                     (temp >= 20) ? 'Wear shorts' :
                       'Might not want to bother going outside'
               }</Text>
-              <Text>Shoes: {
+              <Text style={styles.text}>Shoes: {
                 (temp <= 5) ? 'Wear some warm shoes' :
                   (temp <= 15) ? 'Wear closed shoes' :
                     (temp <= 20) ? 'Wear some breatheable sneakers' :
                       (temp >= 20) ? 'You can wear open slippers or breatheeable sneakers' :
                         'Might not want to bother going outside'
               }</Text>
-              <Text>Umbrella: {
+              <Text style={styles.text}>Umbrella: {
                 (humidity >= 95) ? 'Bring an umbrella!' :
                   (humidity >= 85) ? 'There is a chance you need an umbrella' :
                     (humidity >= 50) ? 'There is  a small chance for rain' :
@@ -88,12 +92,27 @@ export default class LinksScreen extends React.Component {
               }
               </Text>
             </View>
-            <View className="footer">
-              <Button title="Press Me">Try it out!</Button>
+            <View>
+              <Text>
+                <Text style={styles.text}>
+                  {isLoading ? 'loading.....' : response.main.temp}°C &nbsp;
+                 </Text>
+                <Text style={styles.textOrange}>
+                  / &nbsp; {isLoading ? 'loading.....' : response.name} &nbsp;
+                </Text>
+                <Text style={styles.text}>
+                  / &nbsp; {isLoading ? 'loading.....' : response.main.humidity}%
+                </Text>
+              </Text>
             </View>
+          </ScrollView>
+          <View style={styles.footer}>
+            <Button 
+            color= '#FF4D18' 
+            title="Try Again">
+            </Button>
           </View>
-        </View>
-      </ScrollView>
+      </View>
     );
   }
 }
@@ -102,9 +121,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
   },
   mainContainer: {
-    alignItems: 'center'
+    alignItems: 'center',
+    margin: '5%',
   },
+  text: {
+    color: 'white'
+  },
+  textOrange: {
+    color: '#FF4D18',
+  },
+  footer: {
+    // flexDirection: 'row',
+    // justifyContent: 'center',
+    // width: '100%',
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  header: {
+
+  },
+  
 });
